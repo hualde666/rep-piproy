20145c89cc4c35527cac3a20c65d582574f1238f
hualde666 <hualdemirene@gmail.com>
2021-08-25 14:58:59 -0400

    Advertencia de SIN contactos emergencia

--- lib/scr/pages/botonrojo_page.dart
+++ lib/scr/pages/botonrojo_page.dart
@@ -1,7 +1,9 @@
+import 'package:contacts_service/contacts_service.dart';
 import 'package:flutter/material.dart';
 
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:piproy/scr/models/items_lista_contactos.dart';
 
 import 'package:sendsms/sendsms.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -10,84 +12,180 @@ import 'package:flutter_open_whatsapp/flutter_open_whatsapp.dart';
 
 //import 'package:whatsapp_unilink/whatsapp_unilink.dart';
 //import 'package:url_launcher/url_launcher.dart';
-class BotonRojoPage extends StatelessWidget {
+class BotonRojoPage extends StatefulWidget {
+  @override
+  State<BotonRojoPage> createState() => _BotonRojoPageState();
+}
+
+class _BotonRojoPageState extends State<BotonRojoPage> {
+  List<String> listaIdContacto = [];
+
+  List<ItemListaEmergencia> listaE = [];
+
+  List<Contact> listaContactos = [];
+  bool hayLista = true;
+
+  @override
+  void initState() {
+    super.initState();
+    cargarPrefs();
+  }
+
+  Future cargarPrefs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    listaIdContacto = prefs.getStringList('listaE');
+    if (listaIdContacto.isEmpty) {
+      listaIdContacto = [];
+      hayLista = false;
+    }
+    return listaIdContacto;
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Emergencia'),
-          backgroundColor: Color.fromRGBO(55, 57, 84, 1.0),
-        ),
-        body: Center(
-          child: Container(
-            height: 400.0,
-            margin: EdgeInsets.symmetric(horizontal: 10.0),
-            alignment: Alignment.center,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text(
-                  'EMERGENCIA ',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 40.0,
-                  ),
-                ),
-                Text(
-                  'Enviar  mensaje de emergencia a familiares',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30.0,
-                  ),
-                ),
-                SizedBox(
-                  height: 50.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    ElevatedButton(
-                      child: Text('Enviar',
-                          style: TextStyle(
-                            fontSize: 25.0,
-                            color: Theme.of(context).primaryColor,
-                          )
-                          // Theme.of(context).primaryColor),
-                          ),
-                      onPressed: () {
-                        mandarSMS();
+        child: Scaffold(
+      appBar: AppBar(
+        title: Text('Alerta de Emergencia'),
+        backgroundColor: Color.fromRGBO(55, 57, 84, 1.0),
+      ),
+      body: FutureBuilder(
+          future: cargarPrefs(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              return hayLista
+                  ? conListaEmergenia(context)
+                  : sinListaEmergenia(context);
+            }
+          }),
+    ));
+  }
+}
 
-                        Navigator.pop(context);
-                      },
-                    ),
-                    ElevatedButton(
-                      style: ButtonStyle(),
-                      child: Text('Cancelar',
-                          style: TextStyle(
-                              fontSize: 25.0,
-                              color: Theme.of(context).primaryColor)),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                  ],
-                )
-              ],
-            ),
-            decoration: BoxDecoration(
-              color: Color.fromRGBO(200, 0, 0, 1.0),
-              borderRadius: BorderRadius.circular(20.0),
-              // border: Border.all(color: Colors.white, width: 4.0)
+conListaEmergenia(BuildContext context) {
+  return Center(
+    child: Container(
+      height: 400.0,
+      margin: EdgeInsets.symmetric(horizontal: 10.0),
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text(
+            'EMERGENCIA ',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 40.0,
             ),
           ),
-        ),
+          Text(
+            'Enviar  mensaje de emergencia a familiares',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30.0,
+            ),
+          ),
+          SizedBox(
+            height: 50.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                child: Text('Enviar',
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      color: Theme.of(context).primaryColor,
+                    )
+                    // Theme.of(context).primaryColor),
+                    ),
+                onPressed: () {
+                  mandarSMS();
+
+                  Navigator.pop(context);
+                },
+              ),
+              ElevatedButton(
+                style: ButtonStyle(),
+                child: Text('Cancelar',
+                    style: TextStyle(
+                        fontSize: 25.0, color: Theme.of(context).primaryColor)),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          )
+        ],
       ),
-    );
-  }
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(200, 0, 0, 1.0),
+        borderRadius: BorderRadius.circular(20.0),
+        // border: Border.all(color: Colors.white, width: 4.0)
+      ),
+    ),
+  );
+}
+
+sinListaEmergenia(BuildContext context) {
+  return Center(
+    child: Container(
+      height: 400.0,
+      margin: EdgeInsets.symmetric(horizontal: 10.0),
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text(
+            'ADVERTENCIA',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 40.0,
+            ),
+          ),
+          Text(
+            'Debe registrar sus contactos de emergencia para poder notificar la emergencia',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 30.0,
+            ),
+          ),
+          SizedBox(
+            height: 50.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                style: ButtonStyle(),
+                child: Text('Cancelar',
+                    style: TextStyle(
+                        fontSize: 25.0, color: Theme.of(context).primaryColor)),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          )
+        ],
+      ),
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(249, 75, 11, 1),
+        borderRadius: BorderRadius.circular(20.0),
+        // border: Border.all(color: Colors.white, width: 4.0)
+      ),
+    ),
+  );
 }
 
 Future _geoLocal() async {
