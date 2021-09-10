e024eb103b4d753539e804768b028406349267ae
hualde666 <hualdemirene@gmail.com>
2021-09-10 18:26:09 -0400

    guarda mesaje de emergencia con DONE del teclado

--- android/app/src/main/kotlin/com/example/piproy/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/piproy/MainActivity.kt
@@ -1,10 +1,32 @@
-
-
-
 package com.example.piproy
 
+
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+
+import io.flutter.plugin.common.MethodChannel
+//import io.flutter.plugin.common.BinaryMessenger
+
+//import androidx.core.app.ActivityCompat
+//import androidx.core.content.ContextCompat
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import android.os.Bundle
+import android.telephony.SmsManager
+import android.Manifest
+import android.content.pm.PackageManager
 
 class MainActivity: FlutterActivity() {
+    
+      private val CHANNEL = "app.piproy.channel/hualdemirene@gmail.com"
+
     
 }
--- lib/scr/pages/mensaje_emergencia.dart
+++ lib/scr/pages/mensaje_emergencia.dart
@@ -47,75 +47,83 @@ class _EmergenciaMensajeState extends State<EmergenciaMensaje> {
   Widget build(BuildContext context) {
     //cargarPrefs();
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: HeaderApp(
-            context, 'Modificar Mensaje de Emergencia', Text(''), 0.0),
-        body: Center(
-          child: GestureDetector(
-            onTap: () {
-              final FocusScopeNode focus = FocusScope.of(context);
-              if (!focus.hasPrimaryFocus && focus.hasFocus) {
-                FocusManager.instance.primaryFocus.unfocus();
-              }
-            },
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 20.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    constraints: BoxConstraints(maxHeight: 300),
-                    child: TextField(
-                      onTap: () {
-                        final FocusScopeNode focus = FocusScope.of(context);
-                        if (!focus.hasPrimaryFocus && focus.hasFocus) {
-                          FocusManager.instance.primaryFocus.unfocus();
-                        }
-                      },
+      resizeToAvoidBottomInset: false,
+      appBar:
+          HeaderApp(context, 'Modificar Mensaje de Emergencia', Text(''), 0.0),
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            final FocusScopeNode focus = FocusScope.of(context);
+            if (!focus.hasPrimaryFocus && focus.hasFocus) {
+              FocusManager.instance.primaryFocus.unfocus();
+            }
+          },
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  constraints: BoxConstraints(maxHeight: 300),
+                  child: TextField(
+                    onEditingComplete: () {
+                      guardarMensaje();
+                      final FocusScopeNode focus = FocusScope.of(context);
+                      if (!focus.hasPrimaryFocus && focus.hasFocus) {
+                        FocusManager.instance.primaryFocus.unfocus();
+                      }
+                    },
+                    onTap: () {
+                      final FocusScopeNode focus = FocusScope.of(context);
+                      if (!focus.hasPrimaryFocus && focus.hasFocus) {
+                        FocusManager.instance.primaryFocus.unfocus();
+                      }
+                    },
 
-                      style: TextStyle(fontSize: 25.0),
-                      // autofocus: true,
-                      maxLines: null,
-                      keyboardType: TextInputType.text,
-                      controller: _textController,
-                      decoration: InputDecoration(
-                        hintText: _mensaje,
-                        labelText: 'Mensaje de Emergencia:',
-                        border: OutlineInputBorder(),
-                      ),
+                    style: TextStyle(fontSize: 25.0),
+                    // autofocus: true,
+                    maxLines: null,
+                    keyboardType: TextInputType.text,
+                    controller: _textController,
+                    decoration: InputDecoration(
+                      hintText: _mensaje,
+                      labelText: 'Mensaje de Emergencia:',
+                      border: OutlineInputBorder(),
                     ),
                   ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Container(
-                    child: Text(
-                      'Redacte el mensaje que desea enviar a sus contactos de emergencia en caso de un incidente.',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                      textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  child: Text(
+                    'Redacte el mensaje que desea enviar a sus contactos de emergencia en caso de un incidente.',
+                    style: TextStyle(
+                      fontSize: 20,
                     ),
+                    textAlign: TextAlign.center,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
-        floatingActionButton: FloatingActionButton.extended(
-            icon: Icon(Icons.check_circle, color: Colors.white, size: 40),
-            backgroundColor: Color.fromRGBO(249, 75, 11, 1),
-            label: Text('guardar',
-                style: TextStyle(fontSize: 20, color: Colors.white)),
-            tooltip: 'guardar',
-            heroTag: 'guardar',
-            onPressed: () {
-              // *** actualizar mensaje***
-              guardarMensaje();
-              Navigator.pop(context);
-            }));
+      ),
+      // floatingActionButton: FloatingActionButton.extended(
+      // icon: Icon(Icons.check_circle, color: Colors.white, size: 40),
+      // backgroundColor: Color.fromRGBO(249, 75, 11, 1),
+      // label: Text('guardar',
+      //     style: TextStyle(fontSize: 20, color: Colors.white)),
+      // tooltip: 'guardar',
+      // heroTag: 'guardar',
+      // onPressed: () {
+      //   // *** actualizar mensaje***
+      //   guardarMensaje();
+      //   Navigator.pop(context);
+      // })
+    );
   }
 }
